491d945554af22a136c91a9f93984b43a3db998d
tyler-rencher <rencher7@byu.edu>
2026-02-16 19:26:14 -0700

    Phase 3 Getting Started Finished

--- server/src/main/java/server/ServerMain.java
+++ server/src/main/java/server/ServerMain.java
@@ -5,6 +5,8 @@ import chess.*;
 public class ServerMain {
     public static void main(String[] args) {
         var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
+        Server server = new Server();
+        server.run(8080); //Server number
         System.out.println("♕ 240 Chess Server: " + piece);
     }
 }
