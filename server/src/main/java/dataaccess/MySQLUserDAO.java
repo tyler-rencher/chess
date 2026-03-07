7c9777416ce005fecfd832cf42a5f468cf594768
tyler-rencher <rencher7@byu.edu>
2026-03-07 12:26:01 -0700

    Phase 4 UserDAO for SQL finished I think

--- server/src/main/java/dataaccess/MySQLUserDAO.java
+++ server/src/main/java/dataaccess/MySQLUserDAO.java
@@ -1,15 +1,16 @@
 package dataaccess;
 
-import com.google.gson.Gson;
 import model.UserData;
 import org.mindrot.jbcrypt.BCrypt;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.HashSet;
 
 import static java.sql.Statement.RETURN_GENERATED_KEYS;
+import static java.sql.Types.NULL;
 
 public class MySQLUserDAO implements UserDAO{
     HashSet<UserData> userDataSet = new HashSet<>();
@@ -21,58 +22,80 @@ public class MySQLUserDAO implements UserDAO{
     private final String[] createStatements = {
             """
             CREATE TABLE IF NOT EXISTS  userData (
-              `id` int NOT NULL AUTO_INCREMENT,
               `username` varchar(256) NOT NULL,
               `password` varchar(256) NOT NULL,
               `email` varchar(256) NOT NULL,
               `json` TEXT DEFAULT NULL,
-              PRIMARY KEY (`id`),
+              PRIMARY KEY (`username`),
               INDEX(username)
             )
             """
     };
 
     @Override
-    public void clear() {
-        userDataSet.clear();
+    public void clear() throws DataAccessException{
+        var statement = "TRUNCATE userData";
+        executeUpdate(statement);
     }
 
     @Override
     public void createUser(UserData userData) throws DataAccessException{
         var statement = "INSERT INTO userData (username, password, email) VALUES (?, ?, ?)";
         String hashedPassword = BCrypt.hashpw(userData.password(), BCrypt.gensalt());
-        int id = executeUpdate(statement, userData.username(), hashedPassword, userData.email());
+        String id = executeUpdate(statement, userData.username(), hashedPassword, userData.email());
     }
 
     @Override
-    public UserData getUser(String username){
-        for (UserData user : userDataSet) {
-            if (user.username().equals(username)) {
-                return user;
+    public UserData getUser(String username) throws DataAccessException{
+        try (Connection conn = DatabaseManager.getConnection()) {
+            var statement = "SELECT * FROM userData WHERE username=?";
+            try (PreparedStatement ps = conn.prepareStatement(statement)) {
+                try (ResultSet rs = ps.executeQuery()) {
+                    if (rs.next()) {
+                        return readUser(rs);
+                    }
+                }
             }
+        } catch (Exception e) {
+            throw new DataAccessException(String.format("Unable to read data: %s", e.getMessage()));
         }
         return null;
     }
 
-    private int executeUpdate(String statement, UserData userData) throws DataAccessException {
+    private UserData readUser(ResultSet rs) throws SQLException {
+        var username = rs.getString("username");
+        var passwordHashed = rs.getString("password");
+        var email = rs.getString("email");
+        return new UserData(username, passwordHashed,email);
+    }
+
+    private String executeUpdate(String statement, Object... params) throws DataAccessException {
         try (Connection conn = DatabaseManager.getConnection()) {
-            try(PreparedStatement stmt = conn.prepareStatement(statement)) {
-                stmt.setString(1, userData.username());
-                stmt.setString(2, userData.password());
-                stmt.setString(3, userData.email());
-
-                if(stmt.executeUpdate() == 1) {
-                    System.out.println("Updated userData " + userData.getId());
-                } else {
-                    System.out.println(
-                            "Failed to update book " + book.getId());
+            try (PreparedStatement ps = conn.prepareStatement(statement, RETURN_GENERATED_KEYS)) {
+                for (int i = 0; i < params.length; i++) {
+                    Object param = params[i];
+                    switch (param) {
+                        case String p -> ps.setString(i + 1, p);
+                        case Integer p -> ps.setInt(i + 1, p);
+                        case null -> ps.setNull(i + 1, NULL);
+                        default -> {
+                            ps.setNull(i + 1, NULL);
+                        }
+                    }
                 }
-            } catch(SQLException ex) {
-                // ERROR
-                throw DataAccessException("UR MOM GAY");
-            }
+                ps.executeUpdate();
 
+                ResultSet rs = ps.getGeneratedKeys();
+                if (rs.next()) {
+                    return rs.getString(1);
+                }
+
+                return null;
+            }
+        } catch (SQLException e) {
+            throw new DataAccessException(String.format("unable to update database: %s, %s", statement, e.getMessage()));
         }
+    }
 
     private void configureDatabase() throws DataAccessException {
         DatabaseManager.createDatabase();
