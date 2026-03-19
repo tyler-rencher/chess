610755035f717c4712d56f82f2b732a4b65d737c
tyler-rencher <rencher7@byu.edu>
2026-03-19 12:09:27 -0600

    Phase 5 Register working I think!!!

--- client/src/main/java/client/Client.java
+++ client/src/main/java/client/Client.java
@@ -1,5 +1,6 @@
 package client;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Client {
@@ -22,7 +23,7 @@ public class Client {
 
             try {
                 result = eval(line);
-                System.out.print(BLUE + result);
+                System.out.print(result);
             } catch (Throwable e) {
                 var msg = e.toString();
                 System.out.print(msg);
@@ -31,20 +32,50 @@ public class Client {
         System.out.println();
     }
 
+    public String eval(String input) {
+        try {
+            String[] tokens = input.toLowerCase().split(" ");
+            String cmd = (tokens.length > 0) ? tokens[0] : "help";
+            String[] params = Arrays.copyOfRange(tokens, 1, tokens.length);
+            return switch (cmd) {
+                case "register" -> server.registerUser(params);
+//                case "rescue" -> rescuePet(params);
+//                case "list" -> listPets();
+//                case "signout" -> signOut();
+//                case "adopt" -> adoptPet(params);
+//                case "adoptall" -> adoptAllPets();
+                case "quit" -> "quit";
+                default -> help();
+            };
+        } catch (ResponseException ex) {
+            return ex.getMessage();
+        }
+    }
+
     public String help() {
-        if (loggedIn) {
+        if (!loggedIn) {
             return """
-                    - signIn <yourname>
-                    - quit
+                    1 - Login
+                    2 - Register
+                    3 - Help
+                    4 - Quit
+                    
+                    Type 1 to Login, 2 to Register, 3 for Help and, 4 to Quit
                     """;
         }
         return """
-                - list
-                - adopt <pet id>
-                - rescue <name> <CAT|DOG|FROG|FISH>
-                - adoptAll
-                - signOut
-                - quit
+                1 - List Games
+                2 - Create Game
+                3 - Play Game
+                4 - Observe Game
+                5 - Help
+                6 - Logout
+                
+                Type 1 to List Games, 2 to Create a Game, 3 to Play a Game, 4 to Observe a Game, 5 for Help and, 6 to Logout
                 """;
+    }
+
+    private void printPrompt() {
+        System.out.print("\n" + ">>> " );
     }
 }
--- client/src/main/java/client/ServerFacade.java
+++ client/src/main/java/client/ServerFacade.java
@@ -1,7 +1,75 @@
 package client;
 
-public class ServerFacade {
-    public ServerFacade(String serverUrl){
+import com.google.gson.Gson;
+import model.requests.RegisterRequest;
+import model.results.RegisterResult;
 
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class ServerFacade {
+    private final HttpClient client = HttpClient.newHttpClient();
+    private final String serverUrl;
+
+    public ServerFacade(String url) {
+        serverUrl = url;
+    }
+
+    public String registerUser(String[] params) throws ResponseException {
+        RegisterRequest requestModel = new RegisterRequest(params[0], params[1], params[2]);
+        var request = buildRequest("POST", "/user", requestModel);
+        var response = sendRequest(request);
+        RegisterResult result = handleResponse(response, RegisterResult.class);
+        return result != null ? result.authToken() : null;
+    }
+
+    private HttpRequest buildRequest(String method, String path, Object body) {
+        var request = HttpRequest.newBuilder()
+                .uri(URI.create(serverUrl + path))
+                .method(method, makeRequestBody(body));
+        if (body != null) {
+            request.setHeader("Content-Type", "application/json");
+        }
+        return request.build();
+    }
+
+    private HttpRequest.BodyPublisher makeRequestBody(Object request) {
+        if (request != null) {
+            return HttpRequest.BodyPublishers.ofString(new Gson().toJson(request));
+        } else {
+            return HttpRequest.BodyPublishers.noBody();
+        }
+    }
+
+    private HttpResponse<String> sendRequest(HttpRequest request) throws ResponseException {
+        try {
+            return client.send(request, HttpResponse.BodyHandlers.ofString());
+        } catch (Exception ex) {
+            throw new ResponseException(ResponseException.Code.ServerError, ex.getMessage());
+        }
+    }
+
+    private <T> T handleResponse(HttpResponse<String> response, Class<T> responseClass) throws ResponseException {
+        var status = response.statusCode();
+        if (!isSuccessful(status)) {
+            var body = response.body();
+            if (body != null) {
+                throw ResponseException.fromJson(body);
+            }
+
+            throw new ResponseException(ResponseException.fromHttpStatusCode(status), "other failure: " + status);
+        }
+
+        if (responseClass != null) {
+            return new Gson().fromJson(response.body(), responseClass);
+        }
+
+        return null;
+    }
+
+    private boolean isSuccessful(int status) {
+        return status / 100 == 2;
     }
 }
